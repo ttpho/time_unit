c4faf9b526d785ba171f575a0ad700ac96dd1e41
Pho Tran <tranthanhpho2011@gmail.com>
2020-07-17 09:13:33 +0700

    First commit

---
+++ lib/time_unit.ex
@@ -0,0 +1,18 @@
+defmodule TimeUnit do
+  @moduledoc """
+  Documentation for `TimeUnit`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TimeUnit.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/time_unit_test.exs
@@ -0,0 +1,8 @@
+defmodule TimeUnitTest do
+  use ExUnit.Case
+  doctest TimeUnit
+
+  test "greets the world" do
+    assert TimeUnit.hello() == :world
+  end
+end
